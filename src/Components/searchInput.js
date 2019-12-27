9401a2a8ee68cc8e41f3608440b90836b0d40547
DJIDJIK Adel <ea_djidjik@esi.dz>
2019-12-27 14:13:39 +0100

    search input ahow and hide event

--- src/Components/searchInput.js
+++ src/Components/searchInput.js
@@ -1,7 +1,12 @@
 import React, { useState, useRef } from "react";
 
-export default function InputSearch({ widthCol = 4, onSearch }) {
+export default function InputSearch({
+  widthCol = 4,
+  onSearch,
+  animated = false
+}) {
   const inputRef = useRef("");
+  const [open, setOpen] = useState(false);
   const handleKeyPress = e => {
     let value = inputRef.current.value;
     if (e.key === "Enter") {
@@ -9,29 +14,64 @@ export default function InputSearch({ widthCol = 4, onSearch }) {
     }
   };
 
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className={`col-lg-${widthCol}`}>
-      <img
-        className="icon"
-        src="Icon/icons-search-white.png"
-        style={{ position: "absolute", top: 10, left: 30 }}
-        alt=""
-      />
-      <input
-        ref={inputRef}
-        className="dark-input"
-        type="text"
-        placeholder="Search anything"
-        // onChange={handleInputChange}
-        onKeyPress={handleKeyPress}
-        // value={CurrentValue}
-      />
-      <img
-        className="icon"
-        src="Icon/icons-close-small-white.png"
-        style={{ position: "absolute", top: 10, marginLeft: "-23pt" }}
-        alt=""
-      />
+      {animated ? (
+            <span className={`search-input-container ${open?"open-input":"close-input"}`}>
+              <img
+                className="icon"
+                src="Icon/icons-search-white.png"
+                style={{ position: "absolute", top: 10, left: 30 }}
+                alt=""
+                onClick={handleOpen}
+              />
+              <input
+                ref={inputRef}
+                className="search-input"
+                type="text"
+                placeholder="Search anything"
+                onKeyPress={handleKeyPress}
+                onBlur={handleClose}
+              />
+              <img
+                className="icon close"
+                src="Icon/icons-close-small-white.png"
+                style={{ position: "absolute", top: 10, marginLeft: "-23pt" }}
+                alt=""
+              />
+        </span>
+      ) : (
+        <span>
+          <img
+            className="icon"
+            src="Icon/icons-search-white.png"
+            style={{ position: "absolute", top: 10, left: 30 }}
+            alt=""
+          />
+          <input
+            ref={inputRef}
+            className="search-input"
+            type="text"
+            placeholder="Search anything"
+            // onChange={handleInputChange}
+            onKeyPress={handleKeyPress}
+            // value={CurrentValue}
+          />
+          <img
+            className="icon"
+            src="Icon/icons-close-small-white.png"
+            style={{ position: "absolute", top: 10, marginLeft: "-23pt" }}
+            alt=""
+          />
+        </span>
+      )}
     </div>
   );
 }
